7181c85826f99d76b6e4e0fe2204b86a273dca3d
User -2023WHPTA\Administrator <kso77649115@gmail.com>
2024-03-13 18:20:54 +0900

    아동 개인 정보 관리 페이지 js,css 1차 완료

--- cproject/src/container/memberdetail/MemberMangement.js
+++ cproject/src/container/memberdetail/MemberMangement.js
@@ -1,27 +1,50 @@
-import MemberSearch from "./MemberSearch";
 import MemberList from "./MemberList";
+import { useState } from "react";
+import Container from "../Container";
+import MemberDetail from "./MemberDetail";
+
+import SearchBox from "../searchbox/SearchBox";
+import { mem_mng } from "../searchbox/searchData"
+import './MemberMangement.css'
 
 function MemberMangement() {
+    const [subMenuArr, setSubMenuArr] = useState([
+        { name: '기본 인적 사항', content: <MemberDetail></MemberDetail> },
+        { name: '특이사항', content: <div>특이사항세부 textarea사용해서 적기만..??</div> }
+    ]);
+
+    const [subCurrentTab, setSubCurrentTab] = useState(0);
 
     return (
+        <div>
+            <SearchBox data={mem_mng} />
 
-        <div className="MemberMangement">
-            <p>아동 관리 전체 화면</p>
-
-            <MemberSearch />
-
-            <form>
-                <div className='memberMainBox'>
-                    <div>
-                        <div>
-                            <span>대상자 목록(총 20건)</span>
-                            <button>excel다운(이벤트넣기)</button>&nbsp;
-                            <button>출력(이벤트넣기)</button>
-                        </div>
-                        <MemberList />
-                    </div>
+            <div className='memberMainBox'>
+                <div style={{
+                    width: '30%',
+                    height: '100%',
+                }}>
+                    <MemberList />
                 </div>
-            </form>
+
+                <div style={{
+                    borderWidth: 1,
+                    borderStyle: 'solid',
+                    borderColor: 'rgb(223, 222, 222)',
+                    marginLeft: '5px',
+                    marginRight: '5px'
+                }}></div>
+
+                <div style={{
+                    width: '70%',
+                    height: '100%'
+                }}>
+                    <Container
+                        menuArr={subMenuArr} setMenuArr={setSubMenuArr}
+                        currentTab={subCurrentTab} setCurrentTab={setSubCurrentTab} mainSub={'sub'}>
+                    </Container>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ cproject/src/container/searchbox/SearchBox.js
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+function SearchBox({ data }) {
+    const [sbVal, setSbVal] = useState(...[data.content.map((o) => { return o.default })]);
+
+    // input 입력시 useState 값 바꿔주는 함수
+    function change(i, e, d) {
+        let c = sbVal;
+
+        if (d !== undefined) {
+            c[i][d] = e.target.value;
+            setSbVal({
+                ...sbVal
+            });
+            
+        } else {
+            c[i] = e.target.value;
+            setSbVal({
+                ...sbVal
+            });
+        }
+    } //change
+
+    // 가져온 data 바탕으로 input/select 생성
+    function inputBox(o, i) {
+        switch (o.type) {
+            case "text":
+                return <input name={o.state} type="text" value={sbVal[i]} onChange={(e) => { change(i,e) }} />;
+            case "date":
+                if (Array.isArray(o.default)) {
+                    return <>
+                        <input name={o.state[0]} type="date" value={sbVal[i][0]} onChange={(e) => { change(i, e, 0) }} />
+                        ~<input name={o.state[1]} type="date" value={sbVal[i][1]} onChange={(e) => { change(i, e, 1) }} />
+                    </>
+                } else {
+                    return <input name={o.state} type="date" value={sbVal[i]} onChange={(e) => { change(i, e) }} />;
+                }
+            case "select":
+                return <>
+                    <select>
+                        <option name={o.state} value="" key="">전체</option>
+                        {
+                            o.default.map((j) => {
+                                return <option value={j.value} key="">{j.name}</option>
+                            })
+                        }
+                    </select>
+                </>;
+            default:
+                break;
+        }
+    } //inputBox
+
+    return (
+        <>
+            <p>{data.name}</p>
+            <form action={data.action} method={data.method}>
+                <div className='searchBox'>
+                    {
+                        data.content.map((o, i) => {
+                            return (
+                                <div>{
+                                    o.esntl ? <span style={{ color: "red" }}>*</span> : null
+                                }{o.name}&nbsp;&nbsp;
+                                    {inputBox(o, i)}
+                                </div>
+                            );
+                        })
+                    }
+                    <div>
+                        <button type="reset">리셋</button>&nbsp;
+                        <button type="button">조회</button>
+                    </div>
+                </div>
+
+            </form>
+        </>
+    );
+} //SearchBox
+
+export default SearchBox;
