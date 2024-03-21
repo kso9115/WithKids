3a46f9cef679a0713f3c4c9004c82400412d78d8
User -2023FIULM\Administrator <img4029@naver.com>
2024-03-21 09:35:40 +0900

    메인 사진 제거

--- cproject/src/components/main/Main.js
+++ cproject/src/components/main/Main.js
@@ -3,7 +3,9 @@ import logo from '../../assets/images/루피.PNG';
 function Main() {
 
     return (
-        <img alt="logo" src={logo} width="1000" height="600" />
+        <>
+            {/* <img alt="logo" src={logo} width="1000" height="600" /> */}
+        </>
     );
 }
 export default Main;
