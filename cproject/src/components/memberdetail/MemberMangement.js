aa1c9458db5ff758b2a2cd64ca4ed60806e4d812
User -2023WHPTA\Administrator <kso77649115@gmail.com>
2024-03-26 18:03:38 +0900

    되돌리기

--- cproject/src/components/memberdetail/MemberMangement.js
+++ cproject/src/components/memberdetail/MemberMangement.js
@@ -1,7 +1,7 @@
 import MemberList from "./MemberList";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ContainerSub from "../container/ContainerSub";
+import ContainerSub from "../container/ContainerSub";   // 메인&서브 분리하여 컴포넌트 재설정
 import MemberDetail from "./MemberDetail";
 import MemberDetailNote from "./MemberDetailNote";
 
@@ -11,11 +11,14 @@ import './MemberMangement.css'
 
 function MemberMangement() {
 
-    // Member 테이블 전체 중 멤버 한명 선택 데이터 : sub탭에 전달, 초기값은 빈 객체
+    // Member DB 전체 중 멤버 한명 선택 데이터 : sub탭에 전달, 초기값은 빈 객체
     const [memDataOne, setMemDataOne] = useState({});
 
-    // member Education DB연결 : sub탭에 전달, 초기값은 빈 객체
+    // Education DB연결 : sub탭에 전달, 초기값은 빈 객체
     const [eduDataOne, setEduDataOne] = useState();
+
+    // memberList 업데이트 변화 감지를 위한 상태값 : 자식으로 전달
+    const [memListUpdate, serMemListUpdate] = useState(true);
 
     // main, sub 탭
     const [subCurrentTab, setSubCurrentTab] = useState(0);
@@ -26,31 +29,10 @@ function MemberMangement() {
     ]
     subMenuArr[0].content = <MemberDetail
         data={memDataOne} eduData={eduDataOne}
-        setData={setMemDataOne} setEduDataOne={setEduDataOne} />;
-    subMenuArr[1].content = <MemberDetailNote />;
-
-    // Education Entity DB 접속 : memSelectOneEdu 매핑
-    // Get 요청을 보낼 시 params 값을 전달해야한다.
-    // useEffect(() => {
-    //     if (memDataOne.memSerial) {
-    //         console.log(memDataOne.memSerial); // 클릭시 들어옴
-    //         // const memSerial = memDataOne.memSerial;
-
-    //         axios
-    //             .get('/api/mem/memSelectOneEdu',{
-    //                 params:{
-    //                     memSerial:memDataOne.memSerial
-    //                 }
-    //             })
-    //             .then(response => {
-    //                 console.log(response.data); // 데이터 전달 확인용
-    //                 setEduDataOne(response.data);
-    //             }).catch(function (err){
-    //                 console.log("error => ",err);
-    //             })
-    //     }
-    // }, [memDataOne.memSerial]);
-
+        setData={setMemDataOne} setEduDataOne={setEduDataOne}
+        memListUpdate={memListUpdate} serMemListUpdate={serMemListUpdate} />;
+    subMenuArr[1].content = <MemberDetailNote
+        memListUpdate={memListUpdate} serMemListUpdate={serMemListUpdate} />;
 
     // Post 요청을 보낼 시 : 학력 데이터 요청전달 후 출력
     useEffect(() => {
@@ -75,7 +57,6 @@ function MemberMangement() {
     // if (!response.data) setMemDataOne({ memSerial: memDataOne.memSerial });
 
 
-
     return (
         <div className='mem_mng'>
             <SearchBox data={mem_mng} />
@@ -86,6 +67,9 @@ function MemberMangement() {
                     height: '100%',
                 }}>
                     <MemberList
+                        //리스트에도 memListUpdate얘를 전달해야하는거아닌가?
+                        // memListUpdate={memListUpdate}
+                        
                         setData={setMemDataOne}
                         // memDataOne={memDataOne}
                         setEduDataOne={setEduDataOne}
@@ -106,7 +90,7 @@ function MemberMangement() {
                     height: '100%'
                 }}>
                     <ContainerSub
-                        menuArr={subMenuArr}
+                        menuArr={subMenuArr}    //
                         // setMenuArr={setSubMenuArr}   // setMenuArr 신규 버전 변경으로 삭제
                         currentTab={subCurrentTab}
                         setCurrentTab={setSubCurrentTab}>
